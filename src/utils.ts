9bc75d770d5a1634e309ffe83f35042730f307f5
Dev <dev@example.com>
2026-07-21 15:10:38 +0000

    Update src/utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,4 @@ import { Transaction, VersionedTransaction } from '@solana/web3.js';
 export function isLegacyTransactionInstance (transaction: TransactionOrVersionedTransaction): transaction is Transaction {
   return (transaction as VersionedTransaction).version === undefined;
 }
+
