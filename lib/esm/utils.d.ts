edacd44af81f66f0671df8fc9fa1c16cca55014c
Dev <dev@example.com>
2026-07-21 13:51:32 +0000

    Update lib/esm/utils.d.ts

--- lib/esm/utils.d.ts
+++ lib/esm/utils.d.ts
@@ -1,3 +1,4 @@
 import { TransactionOrVersionedTransaction } from './types';
 import { Transaction } from '@solana/web3.js';
 export declare function isLegacyTransactionInstance(transaction: TransactionOrVersionedTransaction): transaction is Transaction;
+
